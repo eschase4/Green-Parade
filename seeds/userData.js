cc3a12de1c1eaf418053282d78016deb858ad78a
siutsang <95592154+royshtsang@users.noreply.github.com>
2023-03-16 19:49:35 -0400

    seeds finished

---
+++ seeds/userData.js
@@ -0,0 +1,23 @@
+const { User } = require('../models');
+
+const userData = [
+  {
+    username: 'number1',
+    email: 'number1@gmail.com',
+    password: 'number1',
+  },
+  {
+    username: 'number2',
+    email: 'number2@gmail.com',
+    password: 'number2',
+  },
+  {
+    username: 'number3',
+    email: 'number2@gmail.com',
+    password: 'number3',
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
