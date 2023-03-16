13adf3ca2961b4e32c5f11046738ca961e6f369b
Eli Chase <eschase4@gmail.com>
2023-03-16 19:34:23 -0400

    making progress with event listeners, hopefully

--- controllers/apiRoutes/playlist-routes.js
+++ controllers/apiRoutes/playlist-routes.js
@@ -1,14 +1,22 @@
 const router = require('express').Router();
 const Song = require('../../models');
 
+const addSong = document.getElementById('#addSongBtn');
+console.log(addSong);
 router.get('/', async (req, res) => {});
 
-router.post('/playlist', async (req, res) => {
-  try {
-    const songData = Song.findAll({});
-
-    res.status(200).json(songData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+addSong.addEventListener('click', () => {
+  console.log('event listener works');
+  router.post('/playlist', async (req, res) => {
+    try {
+      const songData = Song.findAll({});
+      console.log('hello');
+      res.status(200).json(songData);
+    } catch (err) {
+      console.log('hello, but in error form');
+      res.status(500).json(err);
+    }
+  });
 });
+
+module.exports = router;
