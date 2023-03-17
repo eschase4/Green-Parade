a5b4a7faef67e73aa10f61edcbdb275b43a5023b
Eli Chase <eschase4@gmail.com>
2023-03-16 21:14:41 -0400

    created addSong.js

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const User = require('../../models');
+const { User, Song } = require('../../models');
 
 router.post('/login', async (req, res) => {
   try {
@@ -11,9 +11,7 @@ router.post('/login', async (req, res) => {
     });
 
     if (!dbUser) {
-      return res
-        .status(404)
-        .json({ message: "User not found!" });
+      return res.status(404).json({ message: 'User not found!' });
     }
 
     const isValidPassword = await dbUser.comparePassword(req.body.password);
@@ -26,7 +24,7 @@ router.post('/login', async (req, res) => {
       req.session.loggedIn = true;
       return res
         .status(200)
-        .json({ message: "You have logged in succesfully!" });
+        .json({ message: 'You have logged in succesfully!' });
     });
   } catch (error) {
     return res.status(500).json({ message: 'There was a problem logging in!' });
@@ -44,6 +42,19 @@ router.post('/signup', async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({ message: 'There was an error signing up!' });
+  }
+});
+
+// /api/playlist
+router.post('/playlist', async (req, res) => {
+  try {
+    console.info(`${req.method} request received to add a review`);
+    const songData = Song.findAll({});
+    console.log('hello');
+    res.status(200).json(songData);
+  } catch (err) {
+    console.log('hello, but in error form');
+    res.status(500).json(err);
   }
 });
 
--- controllers/apiRoutes/playlist-routes.js
+++ controllers/apiRoutes/playlist-routes.js
@@ -1,22 +1,9 @@
 const router = require('express').Router();
 const Song = require('../../models');
 
-const addSong = document.getElementById('#addSongBtn');
+const addSong = document.querySelector('.addSongBtn');
 console.log(addSong);
 router.get('/', async (req, res) => {});
 
-addSong.addEventListener('click', () => {
-  console.log('event listener works');
-  router.post('/playlist', async (req, res) => {
-    try {
-      const songData = Song.findAll({});
-      console.log('hello');
-      res.status(200).json(songData);
-    } catch (err) {
-      console.log('hello, but in error form');
-      res.status(500).json(err);
-    }
-  });
-});
 
 module.exports = router;
