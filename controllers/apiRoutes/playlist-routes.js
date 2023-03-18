b8036b7ad040df6013e2c0a4d581979749d2e587
Eli Chase <eschase4@gmail.com>
2023-03-17 22:42:47 -0400

    css is back in. /controllers/apiroutes/userRoutes.js may be junk

--- controllers/apiRoutes/playlist-routes.js
+++ controllers/apiRoutes/playlist-routes.js
@@ -1,16 +1,22 @@
-// const router = require('express').Router();
-// const Song = require('../../models');
+const router = require('express').Router();
+const Song = require('../../models');
 
-// router.post('/playlist', async (req, res) => {
-//   try {
-//     console.info(`${req.method} request received to add a review`);
-//     // const songData = Song.findAll({});
-//     console.log(req);
-//     // res.status(200).json(songData);
-//   } catch (err) {
-//     console.log('hello, but in error form');
-//     res.status(500).json(err);
-//   }
-// });
+router.post('/playlist', async (req, res) => {
+  try {
+    console.info(`${req.method} request received to add a review`);
+    // const songData = Song.findAll({});
+    const { songName, artistName, albumArt } = req.body;
+    const postSongData = {
+      songName,
+      artistName,
+      albumArt,
+    };
+    console.log(postSongData);
+    // res.status(200).json(songData);
+  } catch (err) {
+    console.log('hello, but in error form');
+    res.status(500).json(err);
+  }
+});
 
 // module.exports = router;
