7d0c470f062a2960f8ea3d565ce32ed52b42cbf0
Eli Chase <eschase4@gmail.com>
2023-03-20 13:53:12 -0400

    heroku attempt 415

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 const hbs = exphbs.create();
 const PORT = process.env.PORT || 3001;
 
-app.use(session(config.expressSessionConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(config.expressSessionConfig));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
