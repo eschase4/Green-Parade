90789ab325d887a30701c675b3e42aa54ad80198
Eli Chase <eschase4@gmail.com>
2023-03-20 14:17:28 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -13,9 +13,15 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: 'mysql',
     dialect: 'mysql',
     port: 3306,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   }
 );
 // }
